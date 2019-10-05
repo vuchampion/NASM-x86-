3ccbc094df2ca83db8ab370842aef9bcb3126436
vuchampion <vuchampion@csu.fullerton.edu>
2019-10-05 01:15:11 -0700

    Update assembly.asm

--- Assignment 3/assembly.asm
+++ Assignment 3/assembly.asm
@@ -8,22 +8,11 @@ segment .data
 
 intro_message db "Welcome, this program was built by Darren Vu", 10, 0
 intro_message2 db "Let us compute using integers only. ", 0
-random_message db "This is a test message.", 10, 0
-goodbye_message db "I hope you've like this program.", 10, 0
-end_message db "The integer 5 will be returned to the calling program", 10, 0
 prompt db "Enter a sequence of long integers seperated by white space. ", 0
 prompt2 db "After the last input press Control+D.", 10, 0
-confirmation db "You entered %d", 10, 0
-sum_result db "The sum of these two integers is %d.", 10, 0
-product_result db "The product of these two integers is %d.", 10, 0
 failed_conditional db "You have selected to not repeat the program", 10, 0
-passed_conditional db "You have chosen to repeat the program", 10, 0
 integer_f db "%d", 0
 char_f db "%c", 0
-string_f db "%s", 0
-yes db "y", 0
-no db "n", 0
-string_confirmation db "Your char was: %c", 10, 0
 print_value db "The value that lies in this register is: %d", 10, 0
 print_total_numbers db "The amount of numbers entered was: %d", 10, 0
 print_mean db "The integer mean was: %d", 10, 0
@@ -54,6 +43,17 @@ call printf
 mov r15, 0      ;Initialize the loop-counter to zero
 mov r14, 0      ;Clear whatever lies in register r14
 mov r13, 0
+mov r8, 0
+
+repeat:
+mov rax, 0
+mov rdi, prompt
+call printf
+
+mov r15, 0      ;Initialize the loop-counter to zero
+mov r14, 0      ;Clear whatever lies in register r14
+mov r13, 0
+mov r8, 0
 
 loop:           ;Start the loop
 push qword 0    ;Scanf some integers
@@ -102,37 +102,13 @@ call scanf
 mov r13, [rsp]
 pop rax
 
-;mov rax, 0
-;mov rdi, print_value
-;mov rsi, r13
-;call printf
-
 cmp r13, 'y'
-je loopagain
+je repeat
 mov rax, 0
 mov rdi, failed_conditional
 call printf
 jmp continue
 
-loopagain:
-mov rax, 0
-mov rdi, passed_conditional
-call printf
-
 continue:
-
-
-
-;mov rax, 0
-;mov rdi, print_value     ;uncomment this block of code to
-;mov rsi, r14             ;print the value of the sum of all
-;call printf              ;scanned numbers
-
-;mov rax, 0
-;mov rdi, print_total_numbers     ;uncomment this block of code to
-;mov rsi, r15                     ;print out the amounf of scanned numbers
-;call printf
-
 mov rax, 0
-
 ret
